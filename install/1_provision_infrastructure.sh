cc6545601a39bf6ab6bfecbcc3ebd5ea15cb60e3
kslifer <48484195+kslifer@users.noreply.github.com>
2020-10-18 14:13:50 -0400

    Initial staging of changes

--- install/1_provision_infrastructure.sh
+++ install/1_provision_infrastructure.sh
@@ -11,7 +11,8 @@ gcloud config set project $PROJECT_ID
 echo "Enabling Google APIs..."
 gcloud services enable compute.googleapis.com
 gcloud services enable run.googleapis.com
-gcloud services enable vpcaccess.googleapis.com
+# Serverless VPC access for internal-only MySQL connectivity
+#gcloud services enable vpcaccess.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable osconfig.googleapis.com
 gcloud services enable containerregistry.googleapis.com
@@ -33,15 +34,24 @@ fi
 echo "Creating network resources..."
 gcloud compute networks create network --subnet-mode=custom --bgp-routing-mode=global
 gcloud compute networks subnets create subnet --network=network --range=192.168.1.0/28 --region=$REGION --enable-private-ip-google-access
-gcloud compute routers create router --network=network --region=$REGION
-gcloud compute routers nats create nat --router=router --region=$REGION --auto-allocate-nat-external-ips --nat-all-subnet-ip-ranges
-gcloud compute firewall-rules create allow-ssh-ingress-from-iap --network=network --direction=INGRESS --action=allow --rules=tcp:22 --source-ranges=35.235.240.0/20
-gcloud compute networks vpc-access connectors create serverless-connect --network=network --region=$REGION --range=192.168.10.0/28
+# Router / NAT for internal IP connectivity to MySQL
+#gcloud compute routers create router --network=network --region=$REGION
+#gcloud compute routers nats create nat --router=router --region=$REGION --auto-allocate-nat-external-ips --nat-all-subnet-ip-ranges
+gcloud compute firewall-rules create allow-ssh-ingress-from-iap --network=network --direction=INGRESS --action=allow --rules=tcp:22 --source-ranges=35.235.240.0/20 --enable-logging
+# Create a firewall rule to allow connectivity to MySQL server
+gcloud compute firewall-rules create allow-mysql-ingress-all --network=network --direction=INGRESS --action=allow --rules=tcp:3306 --source-ranges=0.0.0.0/0 --enable-logging
+# Serverless VPC connector for internal IP connectivity to MySQL
+#gcloud compute networks vpc-access connectors create serverless-connect --network=network --region=$REGION --range=192.168.10.0/28
+# Reserve a static external IP
+gcloud compute addresses create ${MYSQL_VM} --network-tier=PREMIUM --region=$REGION
 
 # Create compute stack
 echo "Creating compute resources..."
 gcloud compute resource-policies create snapshot-schedule ${MYSQL_VM}-backup --max-retention-days=3 --start-time=04:00 --daily-schedule --region=$REGION --storage-location=$REGION
-gcloud compute instances create ${MYSQL_VM} --zone=$ZONE --machine-type=f1-micro --image-project=debian-cloud --image-family=debian-10 --boot-disk-type=pd-standard --boot-disk-size=30GB --no-boot-disk-auto-delete --network=network --subnet=subnet --no-address
+# Without external IP
+#gcloud compute instances create ${MYSQL_VM} --zone=$ZONE --machine-type=f1-micro --image-project=debian-cloud --image-family=debian-10 --boot-disk-type=pd-standard --boot-disk-size=30GB --no-boot-disk-auto-delete --network=network --subnet=subnet --no-address
+# With external IP
+gcloud compute instances create ${MYSQL_VM} --zone=$ZONE --machine-type=f1-micro --image-project=debian-cloud --image-family=debian-10 --boot-disk-type=pd-standard --boot-disk-size=30GB --no-boot-disk-auto-delete --network=network --subnet=subnet --tags=mysql --address=${MYSQL_VM}
 gcloud compute disks add-resource-policies ${MYSQL_VM} --resource-policies=${MYSQL_VM}-backup --zone=$ZONE
 
 # Enable OS Patch Management
