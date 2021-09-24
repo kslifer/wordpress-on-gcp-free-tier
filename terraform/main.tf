971124b7174cca2bf6306b149bae0db7610d3f4a
Kevin Slifer <48484195+kslifer@users.noreply.github.com>
2021-09-24 05:22:35 -0400

    Adding depends to cloudresourcemanager api

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+# Enable the Cloud Resource Manager API
+resource "google_project_service" "gcp_resource_manager_api" {
+  project = var.project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
+
 # Enable Google Cloud Service APIs
 resource "google_project_service" "gcp_services" {
   count   = length(var.gcp_service_list)
@@ -5,4 +11,8 @@ resource "google_project_service" "gcp_services" {
   service = var.gcp_service_list[count.index]
 
   disable_dependent_services = true
+
+  depends_on = [
+    google_project_service.gcp_resource_manager_api,
+  ]
 }
