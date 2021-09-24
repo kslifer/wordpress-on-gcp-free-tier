7f508c6427786f6b457cdb4cc5bd915607cef7be
Kevin Slifer <48484195+kslifer@users.noreply.github.com>
2021-09-24 14:36:55 -0400

    fw rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Enable Google Cloud Service APIs
 resource "google_project_service" "gcp-services" {
-  provider = google
+  provider                   = google
   for_each                   = toset(var.gcp_service_apis)
   service                    = each.key
   disable_dependent_services = true
@@ -8,7 +8,7 @@ resource "google_project_service" "gcp-services" {
 
 # Create GCS Bucket
 resource "google_storage_bucket" "media-bucket" {
-  provider = google
+  provider      = google
   name          = var.media_bucket
   location      = var.region
   storage_class = "STANDARD"
@@ -20,36 +20,69 @@ resource "google_storage_bucket" "media-bucket" {
 
 resource "google_storage_bucket_access_control" "public-media-rule" {
   provider = google
-  bucket = google_storage_bucket.media-bucket.name
-  role   = "READER"
-  entity = "allUsers"
+  bucket   = google_storage_bucket.media-bucket.name
+  role     = "READER"
+  entity   = "allUsers"
 }
 
 # Create Artifact Registry Docker Repository
 resource "google_artifact_registry_repository" "docker-repo" {
-  provider = google-beta
+  provider      = google-beta
   location      = var.region
   repository_id = var.artifact_repo
-  description = "Docker Repository"
-  format = "DOCKER"
+  description   = "Docker Repository"
+  format        = "DOCKER"
 }
 
 # Create network stack
 resource "google_compute_network" "vpc-network" {
-  provider = google
-  name = "network"
+  provider                = google
+  name                    = "network"
   auto_create_subnetworks = "false"
-  routing_mode = "GLOBAL"
+  routing_mode            = "GLOBAL"
 }
 
 resource "google_compute_subnetwork" "vpc-subnet" {
-  name          = "subnet"
-  ip_cidr_range = "192.168.1.0/28"
-  region        = var.region
-  network       = google_compute_network.vpc-network.id
+  name                     = "subnet"
+  ip_cidr_range            = "192.168.1.0/28"
+  region                   = var.region
+  network                  = google_compute_network.vpc-network.id
   private_ip_google_access = "true"
 }
 
+resource "google_compute_firewall" "ingress-ssh-iap" {
+  name      = "allow-ssh-ingress-from-iap"
+  network   = google_compute_network.vpc-network.name
+  direction = "INGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+
+  log_config {
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+}
+
+resource "google_compute_firewall" "ingress-mysql-all" {
+  name      = "allow-mysql-ingress-all"
+  network   = google_compute_network.vpc-network.name
+  direction = "INGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3306"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  log_config {
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+}
 
 
 
@@ -57,7 +90,7 @@ resource "google_compute_subnetwork" "vpc-subnet" {
 # Enable OS Patch Management
 resource "google_compute_project_metadata" "vm-manager" {
   metadata = {
-    enable-guest-attributes  = "TRUE"
-    enable-osconfig = "TRUE"
+    enable-guest-attributes = "TRUE"
+    enable-osconfig         = "TRUE"
   }
 }
