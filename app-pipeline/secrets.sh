6ceea2ae20d5f5cccf07423acabae5c2eb8e4acb
Kevin Slifer <48484195+kslifer@users.noreply.github.com>
2021-09-25 05:33:03 -0400

    app pipeline

--- app-pipeline/secrets.sh
+++ app-pipeline/secrets.sh
@@ -8,18 +8,18 @@ set -eEuo pipefail
 # source ./install/variables.conf
 
 # Echo substitution variables
-echo ${_WORDPRESS_TABLE_PREFIX}
-echo ${_WORDPRESS_DB_NAME}
-echo ${_WORDPRESS_DB_USER}
-echo ${_WORDPRESS_DB_PASSWORD}
-echo ${_MYSQL_VM_EXT_IP}
+echo $WORDPRESS_TABLE_PREFIX
+echo $WORDPRESS_DB_NAME
+echo $WORDPRESS_DB_USER
+echo $WORDPRESS_DB_PASSWORD
+echo $MYSQL_VM_EXT_IP
 
 # Create the directory in the build context
 mkdir -p ./run/secrets
 
 # Generate the Docker Secret files
-echo ${_WORDPRESS_TABLE_PREFIX} > run/secrets/WORDPRESS_TABLE_PREFIX
-echo ${_WORDPRESS_DB_NAME} > run/secrets/WORDPRESS_DB_NAME
-echo ${_WORDPRESS_DB_USER} > run/secrets/WORDPRESS_DB_USER
-echo ${_WORDPRESS_DB_PASSWORD} > run/secrets/WORDPRESS_DB_PASSWORD
-echo ${_MYSQL_VM_EXT_IP} > run/secrets/WORDPRESS_DB_HOST
+echo $WORDPRESS_TABLE_PREFIX > run/secrets/WORDPRESS_TABLE_PREFIX
+echo $WORDPRESS_DB_NAME > run/secrets/WORDPRESS_DB_NAME
+echo $WORDPRESS_DB_USER > run/secrets/WORDPRESS_DB_USER
+echo $WORDPRESS_DB_PASSWORD > run/secrets/WORDPRESS_DB_PASSWORD
+echo $MYSQL_VM_EXT_IP > run/secrets/WORDPRESS_DB_HOST
