aea80ac5266a773b69d520a7b0c5ea659a4d42d2
Kevin Slifer <48484195+kslifer@users.noreply.github.com>
2021-09-25 09:11:47 -0400

    app pipeline

--- app-pipeline/secrets.sh
+++ app-pipeline/secrets.sh
@@ -11,10 +11,4 @@ echo $(cat ./WORDPRESS_TABLE_PREFIX) > ./run/secrets/WORDPRESS_TABLE_PREFIX
 echo $(cat ./WORDPRESS_DB_NAME) > ./run/secrets/WORDPRESS_DB_NAME
 echo $(cat ./WORDPRESS_DB_USER) > ./run/secrets/WORDPRESS_DB_USER
 echo $(cat ./WORDPRESS_DB_PASSWORD) > ./run/secrets/WORDPRESS_DB_PASSWORD
-echo $(cat ./MYSQL_VM_EXT_IP) > ./run/secrets/MYSQL_VM_EXT_IP
-
-pwd
-ls -l
-cd ./run/secrets
-ls -l
-echo $(cat WORDPRESS_TABLE_PREFIX)
+echo $(cat ./MYSQL_VM_EXT_IP) > ./run/secrets/WORDPRESS_DB_HOST
