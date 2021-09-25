1e01d1c7d8000961fcdd080ae47d9f0a52799dca
Kevin Slifer <48484195+kslifer@users.noreply.github.com>
2021-09-25 06:16:03 -0400

    app pipeline

--- app-pipeline/images.sh
+++ app-pipeline/images.sh
@@ -18,57 +18,57 @@ echo $(cat ./RUN_SERVICE)
 # KMS NOTE: "--sort-by" command seems to be broke; modified commands are below this block
 # test - list all images and count
 #echo "Listing image history"
-#gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags --sort-by=UPDATE_TIME
+#gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags --sort-by=UPDATE_TIME
 #echo "Counting image history"
-#gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags --sort-by=UPDATE_TIME  | grep -v DIGEST | wc -l
+#gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags --sort-by=UPDATE_TIME  | grep -v DIGEST | wc -l
 echo "Counting image history"
-gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags  | grep -v DIGEST | wc -l
+gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags  | grep -v DIGEST | wc -l
 
 # demote 'oldest' to ''
-for DIGEST in $(gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags --filter="tags='oldest'" --format='get(DIGEST)'); do
+for DIGEST in $(gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags --filter="tags='oldest'" --format='get(DIGEST)'); do
   echo "Demoting 'oldest' -> '': " + $DIGEST
-  gcloud artifacts docker tags delete -q "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE:oldest"
+  gcloud artifacts docker tags delete -q "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE):oldest"
 done
 
 # demote 'older' to 'oldest'
-for DIGEST in $(gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags --filter="tags='older'" --format='get(DIGEST)'); do
+for DIGEST in $(gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags --filter="tags='older'" --format='get(DIGEST)'); do
   echo "Demoting 'older' -> 'oldest': " + $DIGEST
-  gcloud artifacts docker tags add -q "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE@${DIGEST}" "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE:oldest"
-  gcloud artifacts docker tags delete -q "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE:older"
+  gcloud artifacts docker tags add -q "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE)@${DIGEST}" "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE):oldest"
+  gcloud artifacts docker tags delete -q "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE):older"
 done
 
 # demote 'old' to 'older'
-for DIGEST in $(gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags --filter="tags='old'" --format='get(DIGEST)'); do
+for DIGEST in $(gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags --filter="tags='old'" --format='get(DIGEST)'); do
   echo "Demoting 'old' -> 'older': " + $DIGEST
-  gcloud artifacts docker tags add -q "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE@${DIGEST}" "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE:older"
-  gcloud artifacts docker tags delete -q "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE:old"
+  gcloud artifacts docker tags add -q "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE)@${DIGEST}" "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE):older"
+  gcloud artifacts docker tags delete -q "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE):old"
 done
 
 # demote 'live' to 'old'
-for DIGEST in $(gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags --filter="tags='live'" --format='get(DIGEST)'); do
+for DIGEST in $(gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags --filter="tags='live'" --format='get(DIGEST)'); do
   echo "Demoting 'live' -> 'old': " + $DIGEST
-  gcloud artifacts docker tags add -q "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE@${DIGEST}" "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE:old"
-  gcloud artifacts docker tags delete -q "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE:live"
+  gcloud artifacts docker tags add -q "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE)@${DIGEST}" "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE):old"
+  gcloud artifacts docker tags delete -q "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE):live"
 done
 
 # update 'staged' to 'live'
-for DIGEST in $(gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags --filter="tags='staged'" --format='get(DIGEST)'); do
+for DIGEST in $(gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags --filter="tags='staged'" --format='get(DIGEST)'); do
   echo "Updating 'staged' -> 'live': " + $DIGEST
-  gcloud artifacts docker tags add -q "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE@${DIGEST}" "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE:live"
-  gcloud artifacts docker tags delete -q "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE:staged"
+  gcloud artifacts docker tags add -q "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE)@${DIGEST}" "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE):live"
+  gcloud artifacts docker tags delete -q "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE):staged"
 done
 
 # delete untagged images
-for DIGEST in $(gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags --filter="tags=''" --format='get(DIGEST)'); do
+for DIGEST in $(gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags --filter="tags=''" --format='get(DIGEST)'); do
   echo "Deleting untagged image: " + $DIGEST
-  gcloud artifacts docker images delete -q "$REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE@${DIGEST}"
+  gcloud artifacts docker images delete -q "$(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE)@${DIGEST}"
 done
 
 # KMS NOTE: "--sort-by" command seems to be broke; modified commands are below this block
 # test - list all images and count
 #echo "Listing image history"
-#gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags --sort-by=UPDATE_TIME
+#gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags --sort-by=UPDATE_TIME
 #echo "Counting image history"
-#gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags --sort-by=UPDATE_TIME  | grep -v DIGEST | wc -l
+#gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags --sort-by=UPDATE_TIME  | grep -v DIGEST | wc -l
 echo "Counting image history"
-gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPO/$RUN_SERVICE --include-tags  | grep -v DIGEST | wc -l
+gcloud artifacts docker images list $(cat ./REGION)-docker.pkg.dev/$PROJECT_ID/$(cat ./ARTIFACT_REPO)/$(cat ./RUN_SERVICE) --include-tags  | grep -v DIGEST | wc -l
