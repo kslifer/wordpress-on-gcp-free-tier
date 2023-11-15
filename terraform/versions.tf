f4d107dff3efeeeee6e8ba228662340844b682ac
Kevin Slifer <48484195+kslifer@users.noreply.github.com>
2023-11-15 04:32:30 -0500

    WP/TF Version Upgrades

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.1.0"
+      version = "~> 5.6.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5.1.0"
+      version = "~> 5.6.0"
     }
   }
 }
