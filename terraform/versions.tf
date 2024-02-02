b6ae6dc6914ed85736f01ed08e93fd7da69e1c0f
Kevin Slifer <48484195+kslifer@users.noreply.github.com>
2024-02-02 07:13:47 -0500

    Multiple updates/upgrades

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.6.0, < 1.7.0"
+  required_version = ">= 1.6.0, < 2.0.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.9.0 "
+      version = "~> 5.14.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5.9.0 "
+      version = "~> 5.14.0"
     }
   }
 }
