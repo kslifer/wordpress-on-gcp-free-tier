129da11da6a9cfeb97d641b08978252c043d4ba6
Kevin <48484195+kslifer@users.noreply.github.com>
2025-07-20 16:13:34 -0400

    Updates

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.30.0"
+      version = "~> 6.44.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 6.30.0"
+      version = "~> 6.44.0"
     }
   }
 }
