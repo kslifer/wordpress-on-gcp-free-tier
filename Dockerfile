a90cd29b91e9054270398a2061477667ac173e0a
Kevin <48484195+kslifer@users.noreply.github.com>
2026-05-24 05:12:11 -0400

    WP updates

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Refer to https://hub.docker.com/_/wordpress/ for the latest
-FROM wordpress:6.9.0-php8.4-apache
+FROM wordpress:7.0.0-php8.4-apache
 
 # Inject the default production php configurations
 USER root:root
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 7.12.0"
+      version = "~> 7.33.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 7.12.0"
+      version = "~> 7.33.0"
     }
   }
 }
